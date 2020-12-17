d8055e09c6988c8a2ae9a537236f9a2fd75ea44c
EnzoDaneri <enzodaneri@hotmail.com>
2020-12-17 20:26:06 -0300

    App ready!!!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scan QR in Flutter.
+
+- Funciona correctamente para versiones nuevas de Android. (En el mini J1 no funciona).
+
+- path_provider: Sirve para saber dónde se encuentra el archivo de la base de datos dentro del dispositivo (la BD es un archivo físico y se necesita llegar a él).
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+import 'package:qr_reader/widgets/custom_navigatorBar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.delete_forever),
+              onPressed: () {
+                Provider.of<ScanListProvider>(context, listen: false)
+                    .borrarTodos();
+              })
+        ],
+      ),
+      body: _HomePageBody(),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //GET SELECTED MENU OPT
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    // CONDITIONAL RENDER PAGE
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    //Temporal db
+    // final tempScan = new ScanModel(valor: 'http://google.com');
+    DBProvider.db.getTodosLosScans().then(print);
+
+    //USING SCANLISTPROVIDER
+    final scanListProvider =
+        Provider.of<ScanListProvider>(context, listen: false);
+
+    switch (currentIndex) {
+      case 0:
+        scanListProvider.cargarScanPorTipo('geo');
+        return MapasPage();
+
+      case 1:
+        scanListProvider.cargarScanPorTipo('http');
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
+  }
+}
